fdbb0cd7cd9a3004232052f13fb0638942449c96
Ivan <asgefes1@gmail.com>
2023-10-06 18:38:08 +0000

    Fix scripts
    Fix indentation in 'brain-even' and 'brain-calc'.

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,14 +2,14 @@
 
 from brain_games.games.common_logic import game_main
 from brain_games.games.brain_game_even import (
-        show_rules, ask_question_even, process_data_even, request_response_even
+    show_rules, ask_question_even, process_data_even, request_response_even
 )
 
 
 def main():
     return game_main(
-            show_rules, ask_question_even,
-            request_response_even, process_data_even
+        show_rules, ask_question_even,
+        request_response_even, process_data_even
     )
 
 
