01858ee3b42f02c20b886b809f8b5c35199a0e28
Ivan <asgefes1@gmail.com>
2023-10-17 11:30:21 +0300

    Refactor in progression module

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,16 +4,6 @@ RULE = 'What number is missing in the progression?'
 
 
 def ask_question():
-    progression = generate_progression()
-    char = '..'
-    random_element_i = random.randint(1, 9)
-    correct_answer = progression[random_element_i]
-    progression[random_element_i] = char
-    question_str = ' '.join(map(str, progression))
-    return question_str, str(correct_answer)
-
-
-def generate_progression():
     # number of terms of an arithmetic progression
     n = 10
 
@@ -22,5 +12,15 @@ def generate_progression():
 
     # random difference between terms of an arithmetic progression
     d = random.randint(1, 10)
+    progression = generate_progression(n, a, d)
+    char = '..'
+    random_element_i = random.randint(1, 9)
+    correct_answer = progression[random_element_i]
+    progression[random_element_i] = char
+    question_str = ' '.join(map(str, progression))
+    return question_str, str(correct_answer)
+
+
+def generate_progression(n, a, d):
     progression = [a + d * i for i in range(n)]
     return progression
